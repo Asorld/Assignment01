7f9773b7a063015da8e8c7497d5179281b915f9f
Asorld <asorld@users.noreply.github.com>
2021-02-17 19:43:46 -0800

    Step 15

---
+++ js/script.js
@@ -0,0 +1,74 @@
+// STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2
+// 5;
+// "Hello";
+// true;
+// null;
+
+// STEP 3
+// let num = x * (z-x);
+// let income = (1000 + x) * 12;
+
+// STEP 4
+// let firstName;
+// let lastName;
+// let address;
+// let city;
+// let state;
+// let zipCode;
+// let yourAge;
+// let referralSource;
+// let mayWeContactYou;
+
+// STEP 5
+// firstName = "Evgeny";
+// let city = "San Diego";
+// let firstName = "Evgeny", zipCode = 91942;
+
+// STEP 6
+// let city = "San Diego", year = 1769;
+// alert(city + " been established in " + year + ".");
+
+// STEP 7
+// let myMessage = true + "blood";
+// let myNumber = 10 + false;
+
+// STEP 8
+//Add \ before ' 
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+ 
+// STEP 9
+// let x = null;
+// let y;
+// alert(y);
+
+// STEP 10
+// typeof 'Hello';
+// typeof 5;
+// typeof true;
+
+// STEP 11
+// alert("Hello "+"Evgeny Anisimov, "+"welcome to the Javascript class"+"!")
+
+// STEP 12
+// let name = "Evgeny Anisimov";
+// alert(`Hello ${name}, `+`welcome to the Javascript class!`)
+
+// STEP 13
+// let name = "Evgeny Anisimov";
+// let course = "JavaScript";
+// alert(`Hello ${name}, `+`welcome to the ${course} class!`);
+
+//STEP 14
+// let name = "Evgeny Anisimov";
+// let course = "JavaScript";
+// alert(`Hello ${name}.\nWelcome to the ${course} class!`);
+
+//STEP 15
